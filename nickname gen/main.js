f1a57fad7a083b80cba6cb0befce47bfb0c18899
Mlavalllee <83092482+Mlavalllee@users.noreply.github.com>
2021-05-21 15:20:51 -0600

    Add files via upload

--- nickname gen/main.js
+++ nickname gen/main.js
@@ -4,26 +4,51 @@
 let FNameIn = document.getElementById("first")
 let LNameIn = document.getElementById("last")
 let OutputEl = document.getElementById("output");
-let x = 0;
+let GEN = document.getElementById('btn-1');
+let GENALL = document.getElementById('btn-2');
 
 // Event Listener
-document.getElementById('btn').addEventListener('click', generate)
+GEN.addEventListener('click', Generate);
+GENALL.addEventListener('click', GenerateAll);
+
+// Global variables
+let FNameOut = "";
+let LNameOut = "";
 
 //Array
 let Nick = ["Strong", "Smart", "Intresting", "Shinobi", "Silent", "Loud", "Master", "Immovable", "Unstopable", "Limit Breaker"];
 
 // Nickname Generator functions
-function generate() {
-  x = Math.random()*10;
-  RandomNumGen();
-  console.log(x);
-  let FNameOut = FNameIn.value ;
-  let LNameOut = LNameIn.value;
-  if(FNameOut == ""|| LNameOut == "" ){
+function Generate() {
+  // get first and last name
+  FNameOut = FNameIn.value;
+  LNameOut = LNameIn.value;
+
+  //Generate Random Number
+  let x = 0;
+  x = Math.floor(Math.random()*10);
+
+  //Nickname Output
+  if(FNameOut == ""|| LNameOut == "") {
     OutputEl.innerHTML = "Please Enter your First and Last name"
   }else {
-    OutputEl.innerHTML = FNameOut + " " + LNameOut + " " + "the" + " " + Nick[x];
+    OutputEl.innerHTML = FNameOut + " " + LNameOut + " the " + Nick[x];
   }
 } 
 
+function GenerateAll() {
+  //get first and last name
+  FNameOut = FNameIn.value;
+  LNameOut = LNameIn.value;
+
+  //Clear Output
+  //OutputEl.innerHTML = "";
+  let OutputString = "";
+  //output Nicknames
+
+  for(let i = 0; i < Nick.length; i++) {
+    OutputString += FNameOut + " " + LNameOut + " the " + Nick[i] + ",    ";
+  }
+  OutputEl.innerHTML = OutputString;
+}
 
